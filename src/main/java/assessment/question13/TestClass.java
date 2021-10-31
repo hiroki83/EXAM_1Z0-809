ae775c26253acd275d1024938faa9e403a99c96b
hinoue <h.inoue@zerouno.it>
2021-10-31 09:00:51 +0100

    upload

---
+++ src/main/java/assessment/question13/TestClass.java
@@ -0,0 +1,10 @@
+package assessment.question13;
+import java.io.Console;
+public class TestClass {
+  public static void main(String[] args) {
+    String line;
+    Console c = System.console();
+    if ((line = c.readLine()) != null)
+      System.out.println(line);
+  }
+}
---
+++ src/main/java/assessment/question14/Compiles.java
@@ -0,0 +1,14 @@
+package assessment.question14;
+import java.util.Scanner;
+public class Compiles {
+  class RainException extends Exception {}
+
+  public static void main(String[] args) {
+    //try(Scanner s = new Scanner("rain"); String line = "";) {//DOES NOT COMPILE
+    //  if (s.nextLine().equals("rain"))
+    //    throw new RainException();//DOES NOT COMPILE
+    //} finally {
+    //  s.close();//DOES NOT COMPILE
+    //}
+  }
+}
