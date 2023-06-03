23c746f2e0d916b6a49652c2f38d1e65434e9408
HIROKI INOUE <hirokiinoue@HIROKIs-MacBook-Air.local>
2023-06-03 18:37:23 +0200

    modified directory path because I got new pc

--- src/main/java/chapter9/Chapter9Constants.java
+++ src/main/java/chapter9/Chapter9Constants.java
@@ -4,6 +4,6 @@ public class Chapter9Constants {
   private Chapter9Constants() {}
 
   public static final String ABS_DIR_ROOT = "/";
-  public static final String[] ABS_DIR_PATH = {"Users", "hirokiinoue", "Practice1Z0809", "src", "main", "resources", "chapter9"};
+  public static final String[] ABS_DIR_PATH = {"Users", "hirokiinoue", "git_repositories", "EXAM_1Z0-809", "src", "main", "resources", "chapter9"};
   public static final String[] REL_DIR_PATH = {"src", "main", "resources", "chapter9"};
 }
