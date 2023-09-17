f9aedcc6b792ce7efc190f23b3a83867973ed515
HIROKI INOUE <hiroki83inoue@icloud.com>
2023-09-18 01:40:10 +0200

    update SetupDerbyDatabase

--- src/main/java/reading3rd/chapter10/setupdb_p509/SetupDerbyDatabase.java
+++ src/main/java/reading3rd/chapter10/setupdb_p509/SetupDerbyDatabase.java
@@ -7,17 +7,18 @@ import java.sql.Statement;
 import java.sql.ResultSet;
 
 /**
- * execute command: java -cp "C:\Users\a859432\JDKs\jdk1.8.0_65\db\lib\derby.jar;target/Practice1Z0809-1.0-SNAPSHOT.jar" reading3rd.chapter10.setupdb_p509.SetupDerbyDatabase
+ * execute command(Windows): java -cp "C:\Users\a859432\JDKs\jdk1.8.0_65\db\lib\derby.jar;target/Practice1Z0809-1.0-SNAPSHOT.jar" reading3rd.chapter10.setupdb_p509.SetupDerbyDatabase
+ * execute command(Mac-OS): java -cp "/Library/Java/JavaVirtualMachines/jdk1.8.0_202.jdk/Contents/Home/db/db-derby-10.14.2.0-lib/lib/derby.jar:target/Practice1Z0809-1.0-SNAPSHOT.jar" reading3rd.chapter10.setupdb_p509.SetupDerbyDatabase
 */
 public class SetupDerbyDatabase {
   public static void main(String ... args) throws SQLException {
     String url = "jdbc:derby:zoo;create=true";
     try (Connection conn = DriverManager.getConnection(url);
       Statement stmt = conn.createStatement()) {
-      ResultSet rtnValDropTableSpecies = stmt.executeQuery("DROP TABLE species"); // false
+      //ResultSet rtnValDropTableSpecies = stmt.executeQuery("DROP TABLE species"); // false
       //boolean rtnValDropTableSpecies = stmt.execute("DROP TABLE species"); // false
       //int rtnValDropTableSpecies = stmt.executeUpdate("DROP TABLE species"); // 0
-      int rtnValDropTableAnimal = stmt.executeUpdate("DROP TABLE animal");
+      //int rtnValDropTableAnimal = stmt.executeUpdate("DROP TABLE animal");
       int rtnValCreateTableSpecies = stmt.executeUpdate("CREATE TABLE species ("
       + "id INTEGER PRIMARY KEY, "
       + "name VARCHAR(255), "
@@ -39,8 +40,8 @@ public class SetupDerbyDatabase {
       int rtnValInsertAnimal4 = stmt.executeUpdate("INSERT INTO animal VALUES (4, 1, 'Eddie', '2010-06-08 01:24:00')");
       int rtnValInsertAnimal5 = stmt.executeUpdate("INSERT INTO animal VALUES (5, 2, 'Zoe', '2005-11-12 03:44:00')");
 
-      System.out.println("rntValDropTableSpecies=" + rtnValDropTableSpecies);
-      System.out.println("rntValDropTableAnimal=" + rtnValDropTableAnimal);
+      //System.out.println("rntValDropTableSpecies=" + rtnValDropTableSpecies);
+      //System.out.println("rntValDropTableAnimal=" + rtnValDropTableAnimal);
       System.out.println("rtnValCreateTableSpecies=" + rtnValCreateTableSpecies);
       System.out.println("rtnValCreateTableAnimal=" + rtnValCreateTableAnimal);
       System.out.println("rtnValInsertSpecie1=" + rtnValInsertSpecie1);
