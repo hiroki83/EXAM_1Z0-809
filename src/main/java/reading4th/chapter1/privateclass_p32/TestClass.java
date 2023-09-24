66513a6736a380daf5ac9e6bf1ec4d3d12127923
HIROKI INOUE <hiroki83inoue@icloud.com>
2023-09-25 00:56:34 +0200

    WIP 4th reading chapter1

---
+++ src/main/java/reading4th/chapter1/privateclass_p32/TestClass.java
@@ -0,0 +1,7 @@
+package reading4th.chapter1.privateclass_p32;
+
+//private class TestClass { // DOES NOT COMPILE // DOES NOT COMPILE
+public class TestClass {
+  public static void main(String ... args) {
+  }
+}
